d62f4149cb1b5a548ff826202744c806f32ce2b0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-01-25 08:06:56 -0500

    remove old page

---
+++ setup.sh
@@ -0,0 +1,16 @@
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install --upgrade pip
+
+pip3 install --upgrade sphinx
+pip3 install --upgrade nbsphinx
+pip3 install --upgrade sphinx-copybutton
+pip3 install --upgrade sphinx-toggleprompt
+pip3 install --upgrade furo
+
+pip3 install --upgrade jupyterlab
+pip3 install --upgrade black isort
+pip3 install --upgrade jupyterlab-code-formatter
+
+
---
+++ sphinx/index.rst
@@ -0,0 +1,22 @@
+*Programa*
+
+Data Analytics con Pandas
+################################################################################
+
+
+    .. toctree::
+        :titlesonly:
+        :glob:
+
+        notebooks/0*
+        notebooks/1*
+
+
+
+**Author**:
+
+    | Prof. Juan David Velásquez-Henao, MSc, PhD
+    | Universidad Nacional de Colombia, Sede Medellín.
+    | jdvelasq@unal.edu.co
+
+
