6d667c98cda201bb65403867398ba0a7451b63ab
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-04-21 15:09:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install --quiet --upgrade pip
+pip3 install --quiet -r requirements.txt
 
 
